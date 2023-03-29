494f2f87123312d388bd43b9b45c39745cca42ad
madinasagatova <2021255@student.cct.ie>
2023-03-29 18:41:51 +0100

    new 3d portfolio website

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-pink-700' />
+            <div className='w-1 sm:h-80 h-40 bg-gradient-to-r from-pink-700' />
+        </div>
+         <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-pink-700'>Madina</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I'm a React Frontend Developer. Need to add more info here. Something about your passion <br className="sm:block hidden"/> This is my Portfolio. And here as well </p>
+          </div>
+      </div>
+      <ComputersCanvas/>
+    </section>
+  )
+}
+
+export default Hero;
